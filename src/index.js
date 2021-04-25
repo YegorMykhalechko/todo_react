d2ada82e42f37e4cb48789d840b224cf16d14281
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2021-04-25 18:25:13 +0200

    react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log('Hello')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ToDoList = () => {
+
+  const arrToDoList = ['Run React', 'Build App'];
+  return (
+    <ul>
+      <li>{arrToDoList[0]}</li>
+      <li>{arrToDoList[1]}</li>
+    </ul>
+  );
+};
+
+const AppHeader = () => {
+  return (
+    <h1>ToDo App</h1>
+  );
+};
+
+const SearchPannel = () => {
+  const typeSearch = 'Start Search'
+  const searchStyle = {
+    fontSize: '20px',
+  }
+  return (
+    <input style ={searchStyle} placeholder={typeSearch}></input>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPannel />
+      <ToDoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
